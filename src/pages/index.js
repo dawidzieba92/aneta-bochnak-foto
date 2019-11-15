06a81ede0ad5b4e95e4336e56c306b951014c3db
Dawid Zieba <dawid.zieba@yahoo.pl>
2019-11-15 19:38:36 +0100

    Move header to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,6 +36,20 @@ export default class IndexPage extends React.Component {
     return (
       <Layout>
         <SEO title="Magia zatrzymanej chwili"/>
+        <header className="masthead">
+          <div className="container h-100">
+            <div className="row h-100 align-items-center justify-content-center text-center">
+              <div className="col-lg-10 align-self-end">
+                <h1 className="text-uppercase text-white font-weight-bold">Magia zatrzymanej chwili</h1>
+                <hr className="divider my-4"/>
+              </div>
+              <div className="col-lg-8 align-self-baseline">
+                <p className="text-white-75 font-weight-light mb-5">Najlepsze zdjęcia na świecie</p>
+                <a className="btn btn-primary btn-xl js-scroll-trigger" href="#about" onClick={Scroller.handleAnchorScroll}>Dowiedz się więcej</a>
+              </div>
+            </div>
+          </div>
+        </header>
         <section className="page-section bg-primary" id="about">
           <div className="container">
             <div className="row justify-content-center">
